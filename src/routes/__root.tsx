9ef4a10aa455781478574efe4fb2a6dcfa158983
Pedro-Vercesi <pedrovercesi82@gmail.com>
2026-06-16 12:31:02 -0300

    prep for deploy

---
+++ src/routes/__root.tsx
@@ -0,0 +1,72 @@
+import {
+  Outlet,
+  createRootRouteWithContext,
+  HeadContent,
+  Link,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { QueryClient } from "@tanstack/react-query";
+import "../styles.css";
+import Header from "#/components/Header";
+
+type RouterContext = { queryClient: QueryClient };
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: RootComponent,
+  notFoundComponent: NotFound,
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content:
+          "Share, explore and build on the best startup ideas and side hustles",
+      },
+      {
+        title: "IdeaDrop - Your Idea Hub",
+      },
+    ],
+  }),
+});
+
+function RootComponent() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <HeadContent />
+      <Header />
+      <main className="flex justify-center p-6">
+        <div className="w-full max-w-4xl bg-white rounded-2xl shadow-lg p-8">
+          <Outlet />
+        </div>
+      </main>
+      <TanStackDevtools
+        config={{
+          position: "bottom-right",
+        }}
+        plugins={[
+          {
+            name: "TanStack Router",
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Oppps The page you are looking for does not exist
+      </p>
+      <Link
+        className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+        to="/"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
+}
